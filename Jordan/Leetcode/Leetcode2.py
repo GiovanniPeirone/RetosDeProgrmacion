5fa89f57172dfe195cdacaee5ee5fad37c8fd53d
Jordan Iralde <iraldejordan10@gmail.com>
2025-01-08 21:38:36 -0300

    Leet

---
+++ Jordan/Leetcode/Leetcode2.py
@@ -0,0 +1,18 @@
+l1 = [2,4,3]
+l2 = [5,6,4]
+
+#Out [7,0,8]
+
+# 342(l1) + 465(l2) = 807.
+
+#Esto es de letcode
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+#class Solution:
+#    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+def addTwoNumbers(l1, l2):
+    toStr
