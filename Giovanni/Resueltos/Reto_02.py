bfcefbcf301c862c03db67d7bd90edb96da83cb1
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2025-01-16 17:36:54 -0300

    asd

--- Giovanni/Resueltos/Reto_02.py
+++ Giovanni/Resueltos/Reto_02.py
@@ -3,19 +3,13 @@
 
 
 def ordenador(lista1, lista2):
-    listaFinal = []
-
-    for i in range(lista1 + lista2):
-
-        if lista1 == i:
-            listaFinal += lista1
-        if lista2 == i:
-            listaFinal += lista2
-    return listaFinal
+    listaFinal = lista1 + lista2
+    return sorted(listaFinal)
+    
 
 
-lista1 = [1,2,4]
+lista1 = [1,3,5]
 
-lista2 = [1,3,4]
+lista2 = [1,2,4]
 
 print(ordenador(lista1,lista2))
